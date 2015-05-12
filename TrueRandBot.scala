f6de22f72a021694961407bb92fc3f33d033d53e
Aaron Hu <hypernovagama@gmail.com>
2015-05-12 18:54:36 +0800

    更改正则表达式，测试表达式，更新构造函数
    Signed-off-by: Aaron Hu <hypernovagama@gmail.com>

--- TrueRandBot.scala
+++ TrueRandBot.scala
@@ -2,19 +2,18 @@ import org.jibble.pircbot._
 import scala.util.matching._
 
 class TrueRandBot extends PircBot {
-  def TrueRandBot() {
-    this.setName("TrueRandBot")
-  }
+  this.setName("TrueRandBot")
 
   override def onMessage(channel: String,
                          sender: String,
                          login: String,
                          hostname: String,
                          message: String) = {
-    val reg = new Regex("""^.r (?:([1-9]+)#)?([1-9]*)d([1-9]*)((?:\+[1-9]+)*) ([\p{P}\w\u2e80-\u9fff]*)""")
+    val reg = """^.r (?:([1-9]+[0-9]*)#)?([1-9]?[0-9]*)d([1-9]?[0-9]*)((?:\+[1-9]+)*) ?([\p{P}\w\u2e80-\u9fff]*)""".r
+    println(message)
     message match {
-      case reg(num, dice, face, bonus, desc) => println(num)
-      case _ => println("d")
+      case reg(num, dice, face, bonus, desc) => println(s"Num:$num Dice:$dice Face:$face Bonus:$bonus Desc:$desc")
+      case _ => println(message)
     }
   }
 }
